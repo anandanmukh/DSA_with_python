0712faeefddbbaf5e0ad5497301c35f627870368
Anandan Mukherjee <anandanmukh@gmail.com>
2023-07-02 23:22:23 +0530

    Update timSort.py

--- sorting_algorithms/timSort.py
+++ sorting_algorithms/timSort.py
@@ -134,3 +134,14 @@ def timSort(arr, beginning, ending):
 			yield from merge(arr, left, mid, right)
 		size *= 2
 		
+
+if __name__ == "__main__":
+    import random
+    array = [random.randint(0, 100) for i in range(10)]
+
+    # print the array before and after sorting
+    print(f'The unsorted array: {array}')
+
+    for i in timSort(array, 0, len(array)-1):
+        continue
+    print(f"The sorted array: {array}")
