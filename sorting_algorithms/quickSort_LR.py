0409fdbb2a592839dc937ee3cb781f6999cf5386
Anandan Mukherjee <anandanmukh@gmail.com>
2023-08-06 17:18:26 +0530

    Update quickSort_LR.py

--- sorting_algorithms/quickSort_LR.py
+++ sorting_algorithms/quickSort_LR.py
@@ -52,3 +52,14 @@ def partition(array, low, high):
         if i >= j:
             return j
         array[i], array[j] = array[j], array[i]
+
+if __name__ == "__main__":
+    import random
+    array = [random.randint(0, 100) for i in range(10)]
+
+    # print the array before and after sorting
+    print(f'The unsorted array: {array}')
+
+    for i in quickSort_LR(array, 0, len(array)-1):
+        continue
+    print(f"The sorted array: {array} ")
