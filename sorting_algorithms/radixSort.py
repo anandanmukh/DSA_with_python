24b6c2e43fb766e92f49a4f7e0e7d57cc4e29dc1
Anandan Mukherjee <anandanmukh@gmail.com>
2023-08-08 22:02:07 +0530

    Update radixSort.py

--- sorting_algorithms/radixSort.py
+++ sorting_algorithms/radixSort.py
@@ -63,3 +63,15 @@ def radixSort(array, *args):
         if g==0:
             break
         exp *= 10
+
+
+if __name__ == "__main__":
+    import random
+    array = [random.randint(0, 100) for i in range(10)]
+
+    # print the array before and after sorting
+    print(f'The unsorted array: {array}')
+
+    for i in radixSort(array, 0, len(array)-1):
+        continue
+    print(f"The sorted array: {array} ")
