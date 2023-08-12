87b4011c81552af57f177b2c148d4d29255cee0c
Anandan Mukherjee <anandanmukh@gmail.com>
2023-08-12 13:33:26 +0530

    Update strandSort.py

--- sorting_algorithms/strandSort.py
+++ sorting_algorithms/strandSort.py
@@ -105,3 +105,15 @@ def strandSort(arr,*args):
     size = len(arr)
     
     yield from helper(arr,size,0)
+
+
+if __name__ == "__main__":
+    import random
+    array = [random.randint(0, 100) for i in range(10)]
+
+    # print the array before and after sorting
+    print(f'The unsorted array: {array}')
+
+    for i in strandSort(array, 0, len(array)-1):
+        continue
+    print(f"The sorted array: {array} ")
